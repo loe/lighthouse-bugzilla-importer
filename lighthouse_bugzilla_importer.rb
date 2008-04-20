51eb540904e08bfe080620233d128e018290c485
W. Andrew Loe III <andrew@andrewloe.com>
2008-04-20 15:16:50 -0700

    Better output, User-Agent is unnecessary.
    Fixed matching bug in users and milestones.

--- lighthouse_bugzilla_importer.rb
+++ lighthouse_bugzilla_importer.rb
@@ -15,21 +15,22 @@ module LighthouseImporter
       bugs.each do |b|
         # Build out the XML ticket.
         body = ""
-        body << "<ticket>"
-        body << "<assigned-user-id type=\"integer\">#{user((b/"assigned_to").innerHTML)}</assigned-user-id>"
-        body << "<title>#{(b/"short_desc").innerHTML}</title>"
-        body << "<state>#{status((b/"bug_status").innerHTML)}</state>"
-        body << "<body>#{((b/"long_desc").first/"thetext").innerHTML}</body>"
-        body << "<milestone-id type=\"integer\">#{milestone((b/"version").innerHTML)}</milestone-id>"
+        body << "<ticket>\n"
+        body << " <assigned-user-id type=\"integer\">#{user((b/"assigned_to").innerHTML)}</assigned-user-id>\n"
+        body << " <title>#{(b/"short_desc").innerHTML}</title>\n"
+        body << " <state>#{status((b/"bug_status").innerHTML)}</state>\n"
+        body << " <body>#{((b/"long_desc").first/"thetext").innerHTML}</body>\n"
+        body << " <milestone-id type=\"integer\">#{milestone((b/"version").innerHTML)}</milestone-id>\n"
         body << "</ticket>"
+        
+        puts body
       
         # POST to tickets.xml
         url = URI.parse("http://#{@config['account']}.lighthouseapp.com/projects/#{@config['project']}/tickets.xml")  
         req = Net::HTTP::Post.new(url.path)
         
         headers = {'X-LighthouseToken' => "#{@config['token']}",
-                  'Content-Type' => 'application/xml',
-                  'User-Agent' => 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.0.1) Gecko/20060111 Firefox/1.5.0.1'}
+                  'Content-Type' => 'application/xml'}
         http = Net::HTTP.new(url.host, url.port)
         res = http.post(url.path, body, headers)
         
@@ -39,7 +40,7 @@ module LighthouseImporter
         else
           res.error!
         end
-        
+
       end
     end
   
@@ -52,11 +53,11 @@ module LighthouseImporter
     end
   
     def self.milestone(milestone)
-      @config['milestones'][milestone]
+      @config['milestones']["#{milestone}"]
     end
   
     def self.user(email)
-      @config['users'][email]
+      @config['users']["#{email}"]
     end
   end
 end
